4f96a1c6116c38d09720d663e5b3a326ba4af82a
Paweł Bień <paw1345@gmail.com>
2018-10-25 12:03:42 +0100

    Plik

---
+++ Sen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Twoje imie ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("jak długo spałeś ?");
+            int hoursofSleep = int.Parse(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Hello," + name);
+            if (hoursofSleep > 8)
+            {
+                Console.WriteLine("tak jesteś wypoczety," + name);
+            }
+            else
+            {
+                Console.WriteLine("nie jesteś wypoczety," + name);
+
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+
+            Console.WriteLine();
+
+        }
+    }
+}
